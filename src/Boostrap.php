d9c853051340be9072211e75c48c47ebf2f2d8fd
k3kan <aintbor@gmail.com>
2023-02-08 00:01:05 +0300

    add error handler

--- src/Boostrap.php
+++ src/Boostrap.php
@@ -2,4 +2,21 @@
 
 declare(strict_types = 1);
 
-echo 'Test Boostrap!';
+require __DIR__ . './../vendor/autoload.php';
+
+$environment = 'develop';
+
+/**
+ *  Register the error handler
+ */
+$whoops = new Whoops\Run();
+if ($environment !== 'production') {
+    $whoops->pushHandler(new Whoops\Handler\PrettyPageHandler());
+} else {
+    $whoops->pushHandler(function($e) {
+        echo 'Todo: Friendly error page and send an email to developer';
+    });
+}
+$whoops->register();
+
+throw new Exception();
