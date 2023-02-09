ee9c36e4ca1ec28ceb14fb8f5eacadf64a23455a
k3kan <aintbor@gmail.com>
2023-02-10 00:11:38 +0300

    add route

--- src/Boostrap.php
+++ src/Boostrap.php
@@ -3,12 +3,37 @@
 declare(strict_types = 1);
 
 require __DIR__ . './../vendor/autoload.php';
+use FastRoute\RouteCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = new Request($_GET, $_POST, [], $_COOKIE, $_FILES, $_SERVER);
 $response = new Response('Content', Response::HTTP_OK, ['content-type' => 'text/html']);
 
-$response->setContent('404 - Page not found');
-$response->setStatusCode(Response::HTTP_NOT_FOUND);
-$response->send();
+$dispatcher = \FastRoute\simpleDispatcher(function (RouteCollector $r) {
+    $r->addRoute('GET', '/hello-world', function () {
+        echo 'Hello World';
+    });
+    $r->addRoute('GET', '/another-route', function () {
+        echo 'This works too';
+    });
+});
+
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getPathInfo());
+switch ($routeInfo[0]) {
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        $response->setContent('404 - Page not found');
+        $response->setStatusCode(404);
+        $response->send();
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setContent('405 - Method not allowed');
+        $response->setStatusCode(405);
+        $response->send();
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
+}
